cf3f3fbf0be1aac1884293a4feb4ce31dfda11d5
root <root@myhost.(none)>
2009-07-09 14:00:52 +0300

    tempates/

---
+++ tempates/index.php
@@ -0,0 +1,86 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html >
+ <head>
+  <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
+<jdoc:include type="head" />
+  
+  <link rel="stylesheet" type="text/css" href="css/template.css" />
+  <script type="text/javascript" src="script.js"></script>
+ </head>
+<body>
+
+<div id="art-main">
+<div class="art-Sheet">
+<div class="pic"></div>
+    <div class="art-Sheet-tl"></div>
+    <div class="art-Sheet-tr"></div>
+    <div class="art-Sheet-bl"></div>
+    <div class="art-Sheet-br"></div>
+    <div class="art-Sheet-tc"></div>
+    <div class="art-Sheet-bc"></div>
+    <div class="art-Sheet-cl"></div>
+    <div class="art-Sheet-cr"></div>
+    <div class="art-Sheet-cc"></div>
+    <div class="art-Sheet-body">
+<?php echo"bölge1" ?>
+<div class="art-contentLayout">
+<?php echo"bölge2"  ?>
+<div class="art-sidebar1"><?php echo"bölge3" ?>
+</div>
+<div class="art-<?php echo"bölge4" ?>">
+
+<?php
+  echo"bölge5"
+?>
+<?php echo"bölge6" ?><div class="art-Post">
+    <div class="art-Post-body">
+<div class="art-Post-inner">
+<div class="art-PostContent">
+
+<jdoc:include type="message" />
+
+</div>
+<div class="cleared"></div>
+
+</div>
+
+    </div>
+</div>
+<jdoc:include type="component" />
+
+<?php echo"bölge7" ?>
+<?php echo"bölge8" ?>
+<?php echo"bölge9" ?>
+</div>
+<?php echo"bölge10" ?>
+<div class="art-sidebar2"><?php echo"bölge11" ?>
+</div>
+
+</div>
+<div class="cleared"></div>
+
+<?php echo"bölge12" ?>
+<div class="art-Footer">
+ <div class="art-Footer-inner">
+  <?php echo"bölge13" ?>
+  <div class="art-Footer-text">
+  <?php echo"bölge14" ?>
+<p>Copyright 2009 ---.<br/>
+Ercan KURU.</p>
+
+  
+  <?php echo"bölge15" ?>
+  
+  </div>
+ </div>
+ <div class="art-Footer-background"></div>
+</div>
+
+    </div>
+</div>
+<div class="cleared"></div>
+<p class="art-page-footer"><a href="http://www.pc-didi.at/?p=joomla_templates">Webdesign Tirol</a> designed by pc-didi.</p>
+</div>
+</body> 
+</html>
